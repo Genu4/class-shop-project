f9c2854ffa14354ab489c2caea2cec89d0014863
Gennadiy <glisovuy@ukr.net>
2023-01-08 10:22:03 +0200

    add pages navigation

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,16 @@
 import Button from '@mui/material/Button'
+import { NavLink } from 'react-router-dom'
+import MenuItem from './MenuItem'
 
 type Props = {}
 const Menu = (props: Props) => {
     return (
         <>
-            <Button color="inherit">Home</Button>
-            <Button color="inherit">Products</Button>
-            <Button color="inherit">Payment</Button>
-            <Button color="inherit">Shipping</Button>
-            <Button color="inherit">Cart</Button>
+            <MenuItem to="/">Home</MenuItem>
+            <MenuItem to="/products">Products</MenuItem>
+            <MenuItem to="/payment">Payment</MenuItem>
+            <MenuItem to="/shipping">Shipping</MenuItem>
+            <MenuItem to="/cart">Cart</MenuItem>
         </>
     )
 }
