b51adae80e9063bf826ae2aa0cf4ff236779f589
Gennadiy <glisovuy@ukr.net>
2023-01-29 16:57:50 +0200

    clean some trash

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,22 +1,8 @@
 import CartProductsList from 'components/CartProductsList/CartProductsList'
 import CartTotal from 'components/CartTotal/CartTotal'
-import {
-    getProductsObject,
-    ProductsProps,
-} from 'components/Products/productsArray'
 import { useAppSelector } from 'redux/hooks'
 
-type Props = {
-    productsInCart?: {
-        [id: number]: number
-    }
-    productsObject?: {
-        [id: number]: ProductsProps
-    }
-}
-
 function CartHeader() {
-    
     const productsInCart = useAppSelector((state) => state.productsInCart)
     return (
         <div>
