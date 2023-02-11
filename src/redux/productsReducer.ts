70155f3444d6e087c6f4d9fbf0687c750ea3b851
Gennadiy <glisovuy@ukr.net>
2023-02-11 21:42:34 +0200

    finished

--- src/redux/productsReducer.ts
+++ src/redux/productsReducer.ts
@@ -1,9 +1,8 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-
-export type ProductsProps = {
-    id:number
+type ProductsProps = {
+    id: number
     name: string
     desc: string
     type: string
@@ -12,28 +11,27 @@ export type ProductsProps = {
     image: string
 }
 
-
-const initialState:ProductsProps[]=[]
+const initialState: ProductsProps[] = []
 
 export const fetchProducts = createAsyncThunk<ProductsProps[]>(
-    "products/fetchProducts",
-   async () => {
-    const response = await axios.get("https://run.mocky.io/v3/ea7b7859-1a85-421a-b5df-cd143774ded6")
-    
-    return response.data.products
-   }
+    'products/fetchProducts',
+    async () => {
+        const response = await axios.get(
+            'https://run.mocky.io/v3/ea7b7859-1a85-421a-b5df-cd143774ded6'
+        )
+        return response.data.products
+    }
 )
 
 export const productsSlice = createSlice({
-    name: "products",
+    name: 'products',
     initialState,
-    reducers:{},
+    reducers: {},
     extraReducers: (builder) => {
-        builder.addCase(fetchProducts.fulfilled,(prevState, action) => {
-        return (prevState = action.payload)
+        builder.addCase(fetchProducts.fulfilled, (prevState, action) => {
+            return (prevState = action.payload)
         })
     },
 })
-
 
 export default productsSlice.reducer
